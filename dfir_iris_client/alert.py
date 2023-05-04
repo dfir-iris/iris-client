345498750de94ec08f35d21ee8d13893e4463dc3
whikernel <whitekernel@whitekernel.fr>
2023-05-04 09:28:44 +0200

    [ADD] Getter of an alert

---
+++ dfir_iris_client/alert.py
@@ -0,0 +1,47 @@
+#  IRIS Client API Source Code
+#  contact@dfir-iris.org
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+import json
+import warnings
+
+from requests import Response
+
+from dfir_iris_client.helper.utils import ApiResponse
+from dfir_iris_client.session import ClientSession
+
+
+class Alert(object):
+    """Handles alert operations"""
+
+    def __init__(self, session: ClientSession):
+        """Init
+
+        Args:
+            session (ClientSession): Client session
+        """
+        self._s = session
+
+    def get_alert(self, alert_id: int) -> ApiResponse:
+        """Get an alert
+
+        Args:
+            alert_id (int): Alert id
+
+        Returns:
+            Response: Response object
+        """
+        return self._s.pi_get(f"/alerts/{alert_id}")
+
---
+++ dfir_iris_client/tests/test_alert.py
@@ -0,0 +1,108 @@
+#  IRIS Client API Source Code
+#  contact@dfir-iris.org
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+import datetime
+import json
+from pathlib import Path
+
+import pytest
+
+from dfir_iris_client.admin import AdminHelper
+from dfir_iris_client.alert import Alert
+from dfir_iris_client.case import Case
+from dfir_iris_client.customer import Customer
+from dfir_iris_client.helper.colors import EventWhite
+from dfir_iris_client.helper.report_template_types import ReportTemplateType, ReportTemplateLanguage
+from dfir_iris_client.helper.utils import assert_api_resp, get_data_from_resp, parse_api_data
+from dfir_iris_client.tests.tests_helper import InitIrisClientTest
+
+
+class AlertTest(InitIrisClientTest):
+
+    def setUp(self) -> None:
+        """ """
+        self.alert = Alert(self.session)
+
+    def test_get_alert(self):
+        """ """
+        resp = self.alert.get_alert(1)
+
+        assert bool(assert_api_resp(resp)) is True
+        data = get_data_from_resp(resp)
+
+        assert parse_api_data(data, 'alert_id') == 1
+        assets = parse_api_data(data, 'assets')
+
+        assert isinstance(assets, list)
+
+        assert isinstance(parse_api_data(data, 'alert_severity_id'), int)
+        assert isinstance(parse_api_data(data, 'alert_owner_id'), int) or parse_api_data(data, 'alert_owner_id') is None
+
+        alert_source_content = parse_api_data(data, 'alert_source_content')
+        assert isinstance(alert_source_content, dict)
+
+        comments = parse_api_data(data, 'comments')
+        assert isinstance(comments, list)
+
+        modification_history = parse_api_data(data, 'modification_history')
+        assert isinstance(modification_history, dict)
+
+        customer = parse_api_data(data, 'customer')
+        assert isinstance(customer, dict)
+
+        owner = parse_api_data(data, 'owner')
+        assert isinstance(owner, dict) or owner is None
+
+        assert isinstance(parse_api_data(data, 'alert_source_link'), str)
+
+        cases = parse_api_data(data, 'cases')
+        assert isinstance(cases, list)
+
+        assert isinstance(parse_api_data(data, 'alert_classification_id'), int)
+        assert isinstance(parse_api_data(data, 'alert_source'), str)
+        assert isinstance(parse_api_data(data, 'alert_tags'), str)
+        assert isinstance(parse_api_data(data, 'alert_context'), dict)
+        assert isinstance(parse_api_data(data, 'alert_id'), int)
+
+        severity = parse_api_data(data, 'severity')
+        assert isinstance(severity, dict)
+
+        alert_creation_time = parse_api_data(data, 'alert_creation_time')
+        assert isinstance(alert_creation_time, str)
+
+        classification = parse_api_data(data, 'classification')
+        assert isinstance(classification, dict)
+
+        assert isinstance(parse_api_data(data, 'alert_title'), str)
+        assert isinstance(parse_api_data(data, 'alert_uuid'), str)
+        assert isinstance(parse_api_data(data, 'alert_source_ref'), str)
+        assert isinstance(parse_api_data(data, 'alert_note'), str)
+        assert isinstance(parse_api_data(data, 'alert_customer_id'), int)
+
+        iocs = parse_api_data(data, 'iocs')
+        assert isinstance(iocs, list)
+
+        assert isinstance(parse_api_data(data, 'alert_description'), str)
+        assert isinstance(parse_api_data(data, 'alert_status_id'), int)
+
+        alert_source_event_time = parse_api_data(data, 'alert_source_event_time')
+        assert isinstance(alert_source_event_time, str)
+
+        status = parse_api_data(data, 'status')
+        assert isinstance(status, dict)
+
+        related_alerts = parse_api_data(data, 'related_alerts')
+        assert isinstance(related_alerts, dict)
